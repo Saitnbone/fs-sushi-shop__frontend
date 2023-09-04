927a3d80382c3bdc885d61cb0e4432b1725774a9
Aleksandr Golodin <golodinaleksandr@gmail.com>
2023-09-04 20:18:55 +0200

    Редактирование общее

---
+++ backend/Server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+app.get('/api', (req, res)=>{
+    res.json({
+        message: "Message from backend Server.js"
+    })
+})

8e959b4f3edd4f6b70ed81c8b230ebcb3f6a833f
Aleksandr Golodin <golodinaleksandr@gmail.com>
2023-09-05 18:37:58 +0200

    Создание файлов для конфигурации сети

---
+++ backend/database/dbConfig.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'localhost', // Адрес сервера MySQL
+    user: 'saint_bone_creative_fusion', // Ваше имя пользователя MySQL
+    passvord: '753abc',
+    database: 'ju_sushi_bd'
+});
+
+// Подключение к базе данных
+connection.connect((err)=>{
+    if(err){
+        console.error('Ошибка подключения к базе данных', err);
+        return;
+    }
+    console.log('Подключение к базе данных успешно установлено.')
+})

d2ff3cef1fa3b2c818f68c3cfde020828eb58abb
Aleksandr Golodin <golodinaleksandr@gmail.com>
2023-09-05 19:50:15 +0200

    Редактирование файлов

--- backend/database/dbConfig.js
+++ backend/database/dbConfig.js
@@ -3,8 +3,8 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost', // Адрес сервера MySQL
     user: 'saint_bone_creative_fusion', // Ваше имя пользователя MySQL
-    passvord: '753abc',
-    database: 'ju_sushi_bd'
+    passvord: '753abc', //
+    database: 'ju_sushi_bd' //
 });
 
 // Подключение к базе данных
@@ -12,6 +12,8 @@ connection.connect((err)=>{
     if(err){
         console.error('Ошибка подключения к базе данных', err);
         return;
+    } else {
+        console.log('Подключение к базе данных успешно установлено.')
     }
-    console.log('Подключение к базе данных успешно установлено.')
+    connection.end();
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
 import Category from "./components/Category";
 import Cart from "./components/Cart";

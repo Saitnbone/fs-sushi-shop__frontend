451b5c5a718bebd80940076d9a242989970743e0
Aleksandr Golodin <golodinaleksandr@gmail.com>
2023-09-24 10:08:11 +0200

    Редактирование контроллера для категорий товаров

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -2,11 +2,27 @@ const {Category} = require('../models/models')
 const ApiError = require('../errors/apiErrors')
 
 class categoryController {
+    async create(req, res, next){
+    try {
+            const{name} = req.body;
+            console.log("Received data:", name)
+            if(!name){
+                throw new ApiError.BadRequest("Name is required");
+            }
+            const category = await Category.create({name})
+        return res.json(category)
+        }
+        catch(e){
+            next(ApiError.badRequest(e.message))
+        }
+    } 
+    /*
     async create(req, res){
         const{name} = req.body
         const category = await Category.create({name})
         return res.json(category)
     }
+    */
     async getAll(req, res){
         const categories = await Category.findAll()
         return res.json(categories)
